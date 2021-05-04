c877542d8e4318bbc4f75041a3fea348f44d04b5
Mark Nicholson <wmark.nicholsonu@gmail.com>
2021-05-04 22:57:59 +0300

    User & Team models. Docker that starting Postgres, Hasura and Django

--- InnoCPC/api/models.py
+++ InnoCPC/api/models.py
@@ -3,9 +3,28 @@ from django.db import models
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=150, unique=True)
+    user_id = models.BigIntegerField()  # user_id from Telegram
+    username = models.CharField(max_length=50, unique=True)
+    cf_login = models.CharField(max_length=50, unique=True)
+    timus_login = models.CharField(max_length=50, unique=True)
+    cf_points = models.IntegerField()
+    timus_points = models.IntegerField()
 
     class Meta:
         indexes = [
             models.Index(fields=['username']),
         ]
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class TeamMember(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    team = models.OneToOneField(Team, on_delete=models.CASCADE)
+
--- README.md
+++ README.md
@@ -12,7 +12,8 @@ Required: \
 ```SECRET_KEY``` - Path to database file (you can leave its default value) \
 ```MODE``` - Mode of server may be: DEBUG, PRODUCTION \
 ```POSTGRES_PASSWORD``` - Password for postgres database. It is required because Docker uses it \
-Not required: \
+```BOTNAME``` - Bot alias with which users log in [(You need to setdomain in @Botfather)](https://core.telegram.org/widgets/login) \
+Optional: \
 ```SET_POSTGRES``` - May be 'True' to use postgres as database or 'False' to use sqlite \
 ```POSTGRES_DB_NAME``` - Name of database in postgres \
 ```POSTGRES_USERNAME``` - Username in posrgres
